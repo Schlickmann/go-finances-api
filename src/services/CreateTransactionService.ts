8323836b9feed70a08fb1bf6fcaa9c9814d3e14b
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-12 19:23:37 -0700

    created route to add transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,36 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  type: 'income' | 'outcome';
+  title: string;
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    // const categoryExists = await
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
