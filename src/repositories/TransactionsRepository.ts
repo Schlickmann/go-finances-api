066dbb0d7ea3b9c666308d5e1340ea67de48a9c2
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-12 19:33:20 -0700

    created getBalance method

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,27 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        accumulator[transaction.type] =
+          (accumulator[transaction.type] || 0) + Number(transaction.value);
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
