c7d781bebf12ed957b859c03773f6501fa7585d4
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-12 18:40:00 -0700

    added foreign key to reference category

---
+++ src/database/migrations/1589333730276-AddCategoryIdToTransactions.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCategoryIdToTransactions1589333730276
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'transaction_category',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'transaction_category');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
